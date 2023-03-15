fe296333a7ec79f86ea43827641b709af8f6cc94
Hong You Wong <hwong@u90z06s01.42kl.edu.my>
2023-03-15 18:56:33 +0800

    added test files

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -3,97 +3,104 @@
 #include "Dog.hpp"
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
+#include "Brain.hpp"
 
 int main()
 {
+	// uncomment the following to show that the Animal constructor is
+	// now only usable by the inheritant classes
+
+	// const Animal *animal = new Animal();
+	// animal->makeSound();
+
 	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	const AAnimal	*meta = new AAnimal();
-	std::cout << std::endl;
-	std::cout << "\033[34mTesting\033[0m" << std::endl;
-
-	std::cout << "AAnimal _type: " << meta->getType() << std::endl;
-	meta->makeSound();
-	std::cout << std::endl;
-
-	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	delete meta;
-	std::cout << std::endl;
-
-	std::cout << "-------------------------------------------------------" << std::endl;
-
-	std::cout << std::endl;
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	const AAnimal	*catto = new Cat();
-	std::cout << std::endl;
-
-	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	std::cout << "Cat _type: " << catto->getType() << std::endl;
-	catto->makeSound();
-	std::cout << std::endl;
-
-	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	delete catto;
-	std::cout << std::endl;
-
-	std::cout << "-------------------------------------------------------" << std::endl;
-
-	std::cout << std::endl;
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	const AAnimal	*doggo = new Dog();
-	std::cout << std::endl;
-	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	std::cout << "Dog _type: " << doggo->getType() <<std::endl;
-	doggo->makeSound();
-	std::cout << std::endl;
-	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	delete doggo;
-	std::cout << std::endl;
-
-	std::cout << "-------------------------------------------------------" << std::endl;
-
-	std::cout << std::endl;
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	const WrongAnimal	*wrong_meta = new WrongAnimal();
+	const Animal	*meta[10];
+	for (int i = 0; i < 10; i++)
+	{
+		if (i % 2)
+		{
+			meta[i] = new Cat();
+			if (meta[i] == NULL)
+			{
+				perror("Cat allocation failed");
+				std::cerr << "Exiting process now";
+				exit(1);
+			}
+		}
+		else
+		{
+			meta[i] = new Dog();
+			if (meta[i] == NULL)
+			{
+				perror("Dog allocation failed");
+				std::cerr << "Exiting process now";
+				exit(1);
+			}
+		}
+	}
 	std::cout << std::endl;
 
 	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	std::cout << "AAnimal _type: " << wrong_meta->getType() << std::endl;
-	wrong_meta->makeSound();
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << std::endl;
+		std::cout << "Animal _type: " << meta[i]->getType() << std::endl;
+		meta[i]->makeSound();
+		std::cout << std::endl;
+	}
 	std::cout << std::endl;
 
 	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	delete wrong_meta;
-	std::cout << std::endl;
+	for (int i = 0; i < 10; i++)
+		delete(meta[i]);
 
-	std::cout << "-------------------------------------------------------" << std::endl;
+//THIS PART IS FOR TESTING DEEP COPY ↓
+
+	std::cout << std::endl << std::endl;
+	std::cout << "#### showing that the copy constructor creates a deep copy ####" << std::endl;
+	std::cout << std::endl;
 
 	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	const WrongAnimal	*wrong_catto = new WrongCat();
+	Dog *a = new Dog();
+	// Cat *a = new Cat();
+	if (a == NULL)
+	{
+		perror("Allocation failed");
+		std::cerr << "Exiting the process now." << std::endl;
+		exit(1);
+	}
+
+	a->setIdea(0, "I have to sniff it");
+	a->setIdea(1, "I have to pee on it");
+	a->setIdea(2, "I have to sniff it again");
+	a->setIdea(101, "some shit");
+
+	Dog *b = new Dog(*a);
+	// Cat *b = new Cat(*a);
+	if (b == NULL)
+	{
+		perror("Allocation failed");
+		std::cerr << "Exiting the process now." << std::endl;
+		exit(1);
+	}
 	std::cout << std::endl;
 
-	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	std::cout << "WrongCat _type: " << wrong_catto->getType() <<std::endl;
-	wrong_catto->makeSound();
+	std::cout << "\033[34mTesting a\033[0m" << std::endl;
+	std::cout << "The " << a->getType() << " a has the following ideas: " << std::endl;
+	a->getIdeas();
 	std::cout << std::endl;
 
-	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	delete wrong_catto;
+	std::cout << "\033[34mDeconstructing a\033[0m" << std::endl;
+	delete(a);
 	std::cout << std::endl;
 
-	std::cout << "-------------------------------------------------------" << std::endl;
-
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	const WrongCat	*wrong_catta = new WrongCat();
+	std::cout << "\033[34mTesting b\033[0m" << std::endl;
+	std::cout << "The " << b->getType() << " b has the following ideas: " << std::endl;
+	b->getIdeas();
 	std::cout << std::endl;
 
-	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	std::cout << "WrongCat _type: " << wrong_catta->getType() <<std::endl;
-	wrong_catta->makeSound();
-	std::cout << std::endl;
-
-	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	delete wrong_catta;
-	std::cout << std::endl;
+	std::cout << "\033[34mDeconstructing b\033[0m" << std::endl;
+	delete(b);
 
 	return (0);
 }
